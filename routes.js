3eb3ab69666d09995936868f926e34245feaf2c7
yaminyassin <Yamin2008@live.com>
2020-07-06 22:48:37 +0100

    routes for palces done

--- routes.js
+++ routes.js
@@ -24,11 +24,11 @@ router.get("/parksnearme/:lat/:long", async (req, res) =>{
         var long = req.params.long;
         console.log([lat, long])
         const query = await pool.query(`SELECT st_asgeojson(geo) as geo,
-                                        ROUND((nvagos/nlugares)*100) as Ocupado,
+                                        ROUND(100-(nvagos/nlugares)*100) as Ocupado,
                                         ROUND(st_distance(ST_SetSRID( ST_Point(${long}, ${lat})::geography, 4326),geo::geography))/1000 as dist 
                                         FROM park 
                                         WHERE nvagos <> 0
-                                        ORDER BY dist ASC limit 5`)
+                                        ORDER BY dist ASC limit 8`)
         res.status(200).json(query.rows)
     } catch (error) {
         console.error(error.message)
@@ -44,7 +44,7 @@ router.get("/parksnearme/:lat/:long/:dist", async(req, res) =>{
         var dist = req.params.dist;
         console.log([lat, long, dist])
         const query = await pool.query(`SELECT st_asgeojson(geo) as geo,
-                            ROUND((nvagos/nlugares)*100) as Ocupado,
+                            ROUND(100 - (nvagos/nlugares)*100 ) as Ocupado,
                             ROUND(st_distance(ST_SetSRID( ST_Point(${long}, ${lat})::geography, 4326),geo::geography))/1000 as dist 
                             FROM park 
                             WHERE (nvagos <> 0 AND ROUND(st_distance(ST_SetSRID( ST_Point(${long}, ${lat})::geography, 4326),geo::geography))/1000 > ${dist})
@@ -91,6 +91,41 @@ router.get("/park", async (req, res) =>{
   
 })
 
+router.get("/placesnearme/:lat/:long", async (req, res) =>{
+    try {
+        var lat = req.params.lat;
+        var long = req.params.long;
+        const query = await pool.query(`SELECT st_asgeojson(geo) as geo, name,
+                                        about, category, photo_path,
+                                        ROUND(st_distance(ST_SetSRID( ST_Point(${long}, ${lat})::geography, 4326),geo::geography))/1000 as dist 
+                                        FROM place 
+                                        ORDER BY dist ASC limit 5`)
+        res.status(200).json(query.rows)
+    } catch (error) {
+        console.error(error.message)
+        res.status(400).send(error.message)
+    }
+})
+
+
+router.get("/placesnearme/:lat/:long/:dist", async(req, res) =>{
+    try {
+        var lat = req.params.lat;
+        var long = req.params.long;
+        var dist = req.params.dist;
+        console.log([lat, long, dist])
+        const query = await pool.query(`SELECT st_asgeojson(geo) as geo, name,
+                            about, category, photo_path,
+                            ROUND(st_distance(ST_SetSRID( ST_Point(${long}, ${lat})::geography, 4326),geo::geography))/1000 as dist 
+                            FROM place 
+                            WHERE ROUND(st_distance(ST_SetSRID( ST_Point(${long}, ${lat})::geography, 4326),geo::geography))/1000 > ${dist}
+                            ORDER BY dist ASC limit 5`)
+        res.status(200).json(query.rows)
+    } catch (error) {
+        console.error(error.message)
+        res.status(400).send(error.message)
+    }
+})
 
 /*
 post what you recieved
